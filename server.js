535f41d3885d019aa4d3e01e3bd67bdd48b392d3
Glitch (fcc-issue-tracker-tf) <none>
2019-08-29 00:58:35 +0000

    Complete build. Testing suite needs work.

---
+++ server.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var expect      = require('chai').expect;
+var cors        = require('cors');
+var helmet      = require('helmet')
+var mongo       = require('mongodb').MongoClient
+
+var apiRoutes         = require('./routes/api.js');
+var fccTestingRoutes  = require('./routes/fcctesting.js');
+var runner            = require('./test-runner');
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+
+app.use(helmet.xssFilter())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Sample front-end
+app.route('/:project/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/issue.html');
+  });
+
+//Index page (static HTML)
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API 
+apiRoutes(app);  
+    
+//404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+// Error Handling middleware - taken from exercise tracker
+app.use((err, req, res, next) => {
+
+  let errCode, errMessage
+  if (err.code) {
+    // mongoose validation error
+    errCode =  400 // bad request
+    // report the first validation error
+    errMessage = err.message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+//Start our server and tests!
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 3500);
+  }
+});
+
+
+module.exports = app; //for testing
